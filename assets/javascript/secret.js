18e3d91cfdef904ac5e760538e4c8190be58d230
Roy Reyna <royreyna7@yahoo.com>
2020-03-14 11:54:55 -0500

    secret.js

---
+++ assets/javascript/secret.js
@@ -0,0 +1,8 @@
+// YELP INFO:
+
+// Client ID
+// YhJGUc4uk-ZFRzBOFDkBrQ
+
+
+var API_KEY = "Ot1z1AX0DjhNZ0KF4YLWmXhaUvO6Qdd63B-QNGwtwBPo4Z5K-0MBawVyTGNl0yhWU13zUV4d3PVzC9kbtAvzgKWmbdOddZ1U4GDb6JcxsKEI_3hKB7bungJFu-lsXnYx";
+
