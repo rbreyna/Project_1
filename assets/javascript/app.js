1971aa2d3a1a400e21ae87ecbe06634918ba7eac
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-03-17 22:12:39 -0500

    created on click events with container hide/show functionalities; added basic css to results div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,38 @@
-getYelp("coffee", "San Antonio, TX")
+// getYelp("coffee", "San Antonio")
+
+$("#search-awesome").on("click", function (event) {
+
+    // Added to prevent page load on type="submit" click event
+    event.preventDefault();
+
+    // Capture user inputs: 
+    var search = $("#user-search").val();
+    console.log(search);
+
+    var location = $("#user-location").val();
+    console.log(location);
+
+    // In here, we'll clear the header div
+    $("#header-id").hide();
+
+    // Create "Search again" button
+    var newButton = $("<button>").addClass("btn btn-primary btn-md btn-search")
+    newButton.html("Search For Something Else");
+    newButton.attr("id", "search-again");
+
+    // Maybe append somewhere else??
+    newDiv = $("<div>").addClass("search-again-div");
+    newDiv.append(newButton);
+    $("#button-div-placement").append(newDiv);
+
+    // Call getYelp function
+    getYelp(search, location);
+
+
+});
 
 function getYelp(search, location) {
+
 
     var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + search + "&location=" + location + "$limit=10";
 
@@ -35,10 +67,10 @@ function getYelp(search, location) {
 
                 var yelpLink = sortedBusinesses[i].url;
 
-                $("#yelp-data-output").append(`
+                $("#row-1").append(`
                 
-                <div class="card" style="width: 18rem;">
-                     <img src="${sortedBusinesses[i].image_url}" class="card-img-top" alt="">
+                <div class="card card-align" style="width: 18rem;">
+                     <img src="${sortedBusinesses[i].image_url}" class="card-img-top results-img" alt="">
                         <div class="card-body">
                             <h5 class='card-title' id='business-name'>${sortedBusinesses[i].name}</h5>
                              <p class='card-text'>Rating: ${sortedBusinesses[i].rating}</p>
@@ -65,3 +97,13 @@ function getYelp(search, location) {
 
 
 }
+
+$(document).on("click", "#search-again", function () {
+
+    $("#header-id").show();
+
+    $("#yelp-data-output").empty();
+
+    // *** NOTE: Remember to empty weather div in app2.js
+
+});
